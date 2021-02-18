1607303ab5743294817f24d7f00d6ae9edca62f1
Vishnu Subramanian <johnfedrickenator@gmail.com>
2021-02-18 16:56:24 +0530

    ch

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='jarviscloud',
+      version='0.1',
+      description='API for JarvisCloud users',
+      url='#',
+      author='auth',
+      author_email='author@email.com',
+      license='MIT',
+      packages=['mypackage'],
+      zip_safe=False)
