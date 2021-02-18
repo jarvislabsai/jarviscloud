3058b5c5004115872486e672287c8fba3485a35d
Vishnu Subramanian <johnfedrickenator@gmail.com>
2021-02-18 16:47:16 +0530

    adding base code

---
+++ jarviscloud/jarviscloud.py
@@ -0,0 +1,13 @@
+import requests
+import urllib3
+from pathlib import Path
+urllib3.disable_warnings()
+
+
+path = Path('/home/.jarviscloud/jarvisconfig')
+
+
+def pause():
+    token, id = path.open().read().split()
+    requests.post('https://backenddev.jarvislabs.ai:8000/autopause',
+                  json={'jwt': token, 'id': id}, verify=False)
