a388a2fb6eb20f8853990c7f26444a8d4762bb27
Vishnu Subramanian <johnfedrickenator@gmail.com>
2022-07-13 06:30:45 +0530

    added destroy

--- jarviscloud/jarviscloud.py
+++ jarviscloud/jarviscloud.py
@@ -11,3 +11,8 @@ def pause():
     token, id, user_id = path.open().read().split()
     requests.post('https://backendprod.jarvislabs.ai:8000/pause',
                   json={'jwt': token, 'id': id}, verify=False)
+    
+def destroy():
+    token, id, user_id = path.open().read().split()
+    requests.post('https://backendprod.jarvislabs.ai:8000/destroy',
+                  json={'jwt': token, 'id': id}, verify=False)
