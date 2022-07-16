7d313505e2c04beb78e390d7e00ba63524b9cbde
vishnu kumar <vishnu.techsource@gmail.com>
2022-07-16 18:03:45 +0530

    Added if condition to differntiate the JWT and API check

--- jarviscloud/jarviscloud.py
+++ jarviscloud/jarviscloud.py
@@ -9,10 +9,20 @@ path = Path('/home/.jarviscloud/jarvisconfig')
 
 def pause():
     token, id, user_id = path.open().read().split()
-    requests.post('https://backendprod.jarvislabs.ai:8000/pause',
-                  json={'jwt': token, 'id': id, 'user_id': user_id}, verify=False)
+    if len(token) > 100:
+        requests.post('https://backendprod.jarvislabs.ai:8000/pause',
+                    json={'jwt': token, 'id': id}, verify=False)
+    else:
+        requests.post('https://backendprod.jarvislabs.ai:8000/pause',
+                    json={'jwt': token, 'id': id, 'user_id': user_id}, verify=False)
+
     
 def destroy():
     token, id, user_id = path.open().read().split()
-    requests.post('https://backendprod.jarvislabs.ai:8000/destroy',
-                  json={'jwt': token, 'id': id, 'user_id': user_id}, verify=False)
+    if len(token) > 100:
+        requests.post('https://backendprod.jarvislabs.ai:8000/destroy',
+                    json={'jwt': token, 'id': id}, verify=False)
+    else:
+        requests.post('https://backendprod.jarvislabs.ai:8000/destroy',
+                    json={'jwt': token, 'id': id, 'user_id': user_id}, verify=False)
+
