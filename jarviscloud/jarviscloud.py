c49ed691625f7c9d8f8bb5eb679cadd1c39b6b6f
vishnu kumar <vishnu.techsource@gmail.com>
2022-07-16 11:25:00 +0530

    Allowing the code to pause/destroy based on user_id

--- jarviscloud/jarviscloud.py
+++ jarviscloud/jarviscloud.py
@@ -10,9 +10,9 @@ path = Path('/home/.jarviscloud/jarvisconfig')
 def pause():
     token, id, user_id = path.open().read().split()
     requests.post('https://backendprod.jarvislabs.ai:8000/pause',
-                  json={'jwt': token, 'id': id}, verify=False)
+                  json={'jwt': token, 'id': id, 'user_id': user_id}, verify=False)
     
 def destroy():
     token, id, user_id = path.open().read().split()
     requests.post('https://backendprod.jarvislabs.ai:8000/destroy',
-                  json={'jwt': token, 'id': id}, verify=False)
+                  json={'jwt': token, 'id': id, 'user_id': user_id}, verify=False)
