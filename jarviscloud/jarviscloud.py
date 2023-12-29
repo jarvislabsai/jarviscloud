58ba2de48e72da3659e38c1ae05a5453290e5336
Vishnu Subramanian <johnfedrickenator@gmail.com>
2023-12-29 18:52:27 +0530

    updated backend url

--- jarviscloud/jarviscloud.py
+++ jarviscloud/jarviscloud.py
@@ -5,26 +5,26 @@ urllib3.disable_warnings()
 
 
 path = Path('/home/.jarviscloud/jarvisconfig')
+url = 'https://backend.jarvislabs.net'
 
 
 def pause():
-    #token, id, user_id = path.open().read().split()
+    # token, id, user_id = path.open().read().split()
     token, id, user_id, machine_name = path.open().read().strip().split('\n')
     if len(token) > 100:
-        requests.post('https://backendprod.jarvislabs.ai:8000/pause',
-                    json={'jwt': token, 'id': id}, verify=False)
+        requests.post(f'{url}/pause',
+                      json={'jwt': token, 'id': id}, verify=False)
     else:
-        requests.post('https://backendprod.jarvislabs.ai:8000/pause',
-                    json={'jwt': token, 'id': id, 'user_id': user_id}, verify=False)
+        requests.post(f'{url}/pause',
+                      json={'jwt': token, 'id': id, 'user_id': user_id}, verify=False)
 
-    
+
 def destroy():
-    #token, id, user_id = path.open().read().split()
+    # token, id, user_id = path.open().read().split()
     token, id, user_id, machine_name = path.open().read().strip().split('\n')
     if len(token) > 100:
-        requests.post('https://backendprod.jarvislabs.ai:8000/destroy',
-                    json={'jwt': token, 'id': id}, verify=False)
+        requests.post(f'{url}/destroy',
+                      json={'jwt': token, 'id': id}, verify=False)
     else:
-        requests.post('https://backendprod.jarvislabs.ai:8000/destroy',
-                    json={'jwt': token, 'id': id, 'user_id': user_id}, verify=False)
-
+        requests.post(f'{url}/destroy',
+                      json={'jwt': token, 'id': id, 'user_id': user_id}, verify=False)
